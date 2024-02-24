0596e75ed6bee42eafd4bf7cf6a9356d7196c501
Johanne ESSIERE <essierejohanne1@gmail.com>
2024-02-24 23:48:29 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,5 @@
- puppet manuscript to replace a line in a file on a server
-
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
