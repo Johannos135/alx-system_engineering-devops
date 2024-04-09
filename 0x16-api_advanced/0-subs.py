7fc6d0007950f0e3c6afc5c0bff473031df88b7a
Johanne ESSIERE <essierejohanne1@gmail.com>
2024-04-09 11:46:45 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
-import json
+"""function that queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    username = 'johannos135'
-    password = 'alxafrica'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/johannos135 API Python for Holberton School'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
-    else:
-        return(0)
+    r = requests.get(url, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+    result = r.json()
+    if 'data' not in result:
+        return 0
+    number_subscribers = result['data']['subscribers']
+    return number_subscribers
